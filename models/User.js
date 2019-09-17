95bdaa0199804c98cf67801c95f6512c63256a1c
Manny Cortez Studios <emailme@mannycortez.com>
2019-09-17 05:07:56 -0400

    Added fs and corse - successfully tested all routes and fixed 'delete' bug...ready for front end

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const uuidv1 = require('uuid/v1')
 const crypto = require('crypto')
+const Post = require("./post")
 
 const userSchema = new mongoose.Schema({
     name: {

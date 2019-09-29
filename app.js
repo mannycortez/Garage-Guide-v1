7a0602f46fe1dfe122cc2d282596c308541a3f62
Manny Cortez Studios <emailme@mannycortez.com>
2019-09-29 19:18:37 -0400

    Connected to MongoDB Atlas and installed dotenv

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('dotenv');
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+// Database connection
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'))
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${ err.message }`)
+});
 
 // Routes
 const postRoutes = require('./routes/post');

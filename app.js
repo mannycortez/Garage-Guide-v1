f90ca41f5f679e279b38f2a89e0e78c6f2e7920c
Manny Cortez Studios <emailme@mannycortez.com>
2019-09-29 19:29:16 -0400

    installed mongoose and de-bugged connection error - good to go

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-const mongoose = require('dotenv');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config()

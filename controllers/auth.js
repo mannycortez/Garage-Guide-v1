ca4667e39fb44bcf9d06534e3daddaae5b42070b
Manny Cortez Studios <emailme@mannycortez.com>
2019-09-30 00:52:14 -0400

    Added protected routes using express-jwt and added user friendly error message

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+require('dotenv').config();
+const expressJwt = require('express-jwt');
 const User = require('../models/user');
 
 
@@ -43,4 +44,8 @@ exports.signin = (req, res) => {
 exports.signout = (req, res) => {
     res.clearCookie("t")
     return res.json({ message: "Signout success!"});
-}
+};
+
+exports.requireSignin = expressJwt({
+    secret: process.env.JWT_SECRET
+})

9d1ea9ecb5896f2ae29a3a98982478b83a4653a0
Manny Cortez Studios <emailme@mannycortez.com>
2019-09-29 22:31:23 -0400

    Re-factored routes.  Ready for authentication

--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const postController = require('../controllers/post')
+const express = require('express');
+const { getPosts, createPost } = require('../controllers/post');
 const validator = require('../validator/index');
 
 const router = express.Router()
 
-router.get('/', postController.getPosts);
-router.post('/post', validator.createPostValidator, postController.createPost);
+router.get('/', getPosts);
+router.post('/post', validator.createPostValidator, createPost);
 
 module.exports = router;

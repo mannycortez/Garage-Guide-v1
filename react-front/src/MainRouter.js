d21fbc82f99f4a33a038129fdcae4df84d8f421a
Manny Cortez Studios <emailme@mannycortez.com>
2019-10-01 03:30:34 -0400

    Created nav or menu component and added styles from material

--- react-front/src/MainRouter.js
+++ react-front/src/MainRouter.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './core/Home'
+import Menu from './core/Menu'
 import SignUp from './user/SignUp'
 import SignIn from './user/SignIn'
 
 const MainRouter = () => (
     <div>
+        <Menu />
         <Switch>
             <Route exact path="/" component={ Home } />
             <Route exact path="/signup" component={ SignUp } />

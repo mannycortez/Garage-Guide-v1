b01f62276115025d25733ae63d6298bb87b02e75
Manny Cortez Studios <emailme@mannycortez.com>
2019-09-30 23:27:35 -0400

    Added 'Home' and 'Signup' component. Also added MainRouter component and create basic routes using react-router-dom

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import MainRouter from './MainRouter'
 
-class App extends Component {
-  render() {
-    return (
-      <div className = "container">
-        <h1>React Front-end</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <MainRouter />
+  </BrowserRouter>
+)
 
 export default App;

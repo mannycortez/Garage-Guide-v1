354d43852877a271e06448db29f7adc02107c533
Manny Cortez Studios <emailme@mannycortez.com>
2019-11-16 22:50:19 -0800

    Added GoogleMaps API

--- garage-guide-next-js/pages/map.js
+++ garage-guide-next-js/pages/map.js
@@ -1,17 +1,25 @@
 import React from 'react'
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
 import BaseLayout from '../components/layouts/BaseLayout'
 import BasePage from '../components/BasePage'
 
-class GoogleMap extends React.Component {
-    render() {
-        return (
-            <BaseLayout>
-                <BasePage>
-                   <h1>This is the Google map page.</h1>
-                </BasePage>
-            </BaseLayout>
-        )
-    }
-} 
+function Map() {
+    return <GoogleMap defaultZoom={ 10 } defaultCenter={{ lat: 37.774929, lng: -122.419418 }} />
+}
 
-export default GoogleMap;
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default function App() {
+    return (
+    
+    <div style ={{ width: '100vw', height: '100vh'}}>
+        <WrappedMap 
+        googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
+        loadingElement = {<div style ={{ height: "100%" }}/>}
+        containerElement = {<div style ={{ height: "100%" }}/>}
+        mapElement = {<div style ={{ height: "100%" }}/>}
+        />
+    </div>
+    );
+}
+

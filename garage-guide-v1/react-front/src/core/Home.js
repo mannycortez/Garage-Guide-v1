536a97d65b815d1abd09ce78c59f35e0dd32ebd6
Manny Cortez Studios <emailme@mannycortez.com>
2020-01-01 12:38:38 -0500

    Added post method on homepage

--- garage-guide-v1/react-front/src/core/Home.js
+++ garage-guide-v1/react-front/src/core/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Posts from '../post/Posts'
 
 const Home = () => (
+    <div>
     <div className = "jumbotron">
         <h2>Home</h2>
         <p className="lead">Welcome to React Front end</p>
+    </div>
+    <div className = "container">
+        <Posts />
+    </div>
     </div>
 );
 
